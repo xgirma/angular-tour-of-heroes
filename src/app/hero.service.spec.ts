4ecf7cf73618508631bc35622bc3cedf0e72d443
Girma Nigusse <xgirma@gmail.com>
2019-11-19 23:25:26 -0500

    add test for HeroService

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,20 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import {inject, TestBed} from '@angular/core/testing';
 
 import { HeroService } from './hero.service';
-import {defer} from 'rxjs';
-
-export function fakeAsyncResponse<T>(data: T) {
-  return defer(() => Promise.resolve(data));
-}
-
-const heroServiceStub = {
-  get() {
-    return fakeAsyncResponse([
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' }
-    ]);
-  }
-};
 
 describe('HeroService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -23,4 +9,10 @@ describe('HeroService', () => {
     const service: HeroService = TestBed.get(HeroService);
     expect(service).toBeTruthy();
   });
+
+  it(`get all heroes`, inject([HeroService], (heroService) => {
+    heroService.getHeroes().subscribe(heroes => {
+      expect(heroes.length).toEqual(10);
+    });
+  }));
 });
